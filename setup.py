ef85a73aecc0dd774206faf7b0b9c21102193c99
Pratik Rathod <pr8489822@gmail.com>
2022-05-20 10:34:26 +0530

    SGGS FINAL YEAR PROJECT

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="wafer-fault-detection",
+    version="1",
+    description="ML project",
+    author="Pratik Rathod, Akashay Wadinge, Pramod Shendre",
+    packages=find_packages()
+
+)
